fe1336e17f8a13ae15a8b8e4f318696ce464f823
MW <m@m>
2021-11-17 16:57:20 +0100

    kolejne zmiany

--- mytestsite/movie/models.py
+++ mytestsite/movie/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.core.validators import \
+    MinValueValidator, MaxValueValidator, MinLengthValidator
+from django.core.exceptions import ValidationError
+
 import uuid
 import os
 # Create your models here.
@@ -7,6 +11,17 @@ def custom_file_upload(instance, filename):
     #return f"{str(uuid.uuid4())}/{filename}"
     return os.path.join(str(uuid.uuid4(),filename))
 
+def check_descr(value):
+    if "dupa" in value:
+        raise ValidationError("Ale brzydko piszesz!!!")
+    else:
+        return value
+
+custom_error_msg = {
+    "null" : "To pole ma wartość NULL",
+    "blank" : "To pole nie ma wprowadzonej wartości",
+    "required" : "To pole jest obowiązkowe",
+}
 
 # Model przechowujący dane o filmie
 class Movie(models.Model):
@@ -17,11 +32,14 @@ class Movie(models.Model):
         ('PG-13' , 'Za zgodą rodziców'),
         ('NC-17' , 'Powyżej 17 roku życia'),
     )
-    title = models.CharField(max_length=255, verbose_name="Tytuł filmu")
-    description = models.TextField(default="", verbose_name="Opis")
+    title = models.CharField(max_length=255, verbose_name="Tytuł filmu",
+                             error_messages=custom_error_msg,
+                             validators=[MinLengthValidator(2, message="Tytuł za krótki")])
+    description = models.TextField(default="", verbose_name="Opis", validators=[check_descr])
     released = models.DateField(verbose_name="Data premiery", null=True, blank=True)
     year = models.IntegerField(editable=False, null=True, blank=True)
-    imdb = models.DecimalField(max_digits=4, decimal_places=1, null=True, blank=True)
+    imdb = models.DecimalField(max_digits=4, decimal_places=1, null=True, blank=True,
+                               validators=[MinValueValidator(1), MaxValueValidator(10)])
     mpaa_rating = models.CharField(choices=MPAA, max_length=10,
                                    verbose_name="MPAA", default='G')
     trailer_video = models.URLField(null=True, blank=True, verbose_name="Trailer")
@@ -39,3 +57,15 @@ class Movie(models.Model):
     class Meta:
         verbose_name = "Film"
         verbose_name_plural = "Filmy"
+
+# odbiorniki i sygnały
+from django.db.models import signals
+from django.dispatch import receiver
+
+@receiver(signals.pre_save, sender=Movie)
+def title_upper(sender, instance, **kwargs):
+    instance.title = instance.title.upper()
+
+@receiver(signals.post_save, sender=Movie)
+def after_movie_save(sender, instance, created, **kwargs):
+    print(created)
